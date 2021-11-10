b4bfdd5c2f53f11441c14b3fae1afad5c6589afc
salman602 <salmanrahman602@gmail.com>
2021-11-10 13:15:06 +0600

    added navigation and banner

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
-import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+import Home from './pages/Home/Home/Home';
 
 function App() {
   return (
     <div className="App">
-      
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
---
+++ src/pages/Home/Banner/Banner.js
@@ -0,0 +1,71 @@
+import React from 'react';
+import { Button, Carousel, Container, Col, Row } from 'react-bootstrap';
+import img1 from '../../../assets/banner1.png';
+import img2 from '../../../assets/banner2.png';
+import img3 from '../../../assets/banner3.png';
+
+
+
+const Banner = () => {
+    return (
+        <Carousel variant="dark">
+
+            <Carousel.Item>
+                <Container>
+                    <Row>
+                        <Col xs={12} md={6} className="d-flex justify-content-center align-items-center">
+                            <div>
+                                <h2>STYLISH BACKPACK</h2>
+                                <h5 className="my-3">Browse your backpack now.</h5>
+                                <Button>Browse Now</Button>
+                            </div>
+                        </Col>
+                        <Col xs={12} md={6}>
+                            <img className="mw-100 rounded" src={img2} alt="" height="95%" />
+                        </Col>
+                    </Row>
+                </Container>
+            </Carousel.Item>
+
+            <Carousel.Item >
+                <Container>
+                    <Row>
+                        <Col xs={12} md={6}>
+                            <img className="mw-100 rounded" src={img3} alt="" height="95%" />
+                        </Col>
+                        <Col xs={12} md={6} className="d-flex justify-content-center align-items-center">
+                            <div>
+                                <h2>STYLISH BACKPACK</h2>
+                                <h5 className="my-3">Browse your backpack now.</h5>
+                                <Button>Browse Now</Button>
+                            </div>
+                        </Col>
+
+                    </Row>
+                </Container>
+            </Carousel.Item>
+
+            <Carousel.Item >
+                <Container>
+                    <Row>
+                        <Col xs={12} md={6}>
+                            <img className="mw-100 rounded" src={img1} alt="" height="95%" />
+                        </Col>
+                        <Col xs={12} md={6} className="d-flex justify-content-center align-items-center">
+                            <div>
+                                <h2>STYLISH BACKPACK</h2>
+                                <h5 className="my-3">Browse your backpack now.</h5>
+                                <Button>Browse Now</Button>
+                            </div>
+                        </Col>
+                    </Row>
+                </Container>
+            </Carousel.Item>
+
+        </Carousel>
+    );
+};
+
+export default Banner;
+
+
